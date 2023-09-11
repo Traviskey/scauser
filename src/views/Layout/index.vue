49f6b5876824e6861d5c3e1d5aa0f9d7e704ff91
turkey <1281314239@qq.com>
2023-09-11 17:08:19 +0800

    pinia

--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -3,6 +3,13 @@ import LayoutNav from '@/views/Layout/component/LayoutNav.vue'
 import LayoutHeader from '@/views/Layout/component/LayoutHeader.vue'
 import LayoutFooter from '@/views/Layout/component/LayoutFooter.vue'
 import LayoutFixed from "@/views/Layout/component/LayoutFixed.vue"
+
+import {useCategoryStore} from "@/stores/category";
+import {onMounted} from "vue";
+
+const categoryStore = useCategoryStore();
+
+onMounted(() => categoryStore.getCategory())
 </script>
 
 <template>
